52e939a2a50cd615b994134e6f84912fa3868ef9
Earnest <earnestmuch@gmail.com>
2023-05-30 16:50:26 +0300

    update

--- src/CryptoContext.js
+++ src/CryptoContext.js
@@ -3,12 +3,12 @@ import React, { createContext, useContext, useEffect, useState } from "react";
 const Crypto = createContext();
 
 const CryptoContext = ({ children }) => {
-  const [currency, setCurrency] = useState("INR");
-  const [symbol, setSymbol] = useState("₹");
+  const [currency, setCurrency] = useState("USD");
+  const [symbol, setSymbol] = useState("$");
 
   useEffect(() => {
-    if (currency === "INR") setSymbol("₹");
-    else if (currency === "USD") setSymbol("$");
+    if (currency === "USD") setSymbol("$");
+    else if (currency === "INR") setSymbol("₹");
   }, [currency]);
 
   return (
